ad4365f67fe7b82e6d54722fe07102419c81bb3f
David <david@credo.codes>
2024-06-21 08:58:22 +0200

    tried to get mdns working, currently android causing issues at the moment

--- Assets/_Scripts/mDNS/MDNSDiscoveryClient.cs
+++ Assets/_Scripts/mDNS/MDNSDiscoveryClient.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 using Zeroconf;
 namespace SprayAR
@@ -7,6 +10,15 @@ namespace SprayAR
     public class MDNSDiscoveryClient : MonoBehaviour
     {
         private List<string> _discoveredServices = new List<string>();
+        private AndroidJavaObject multicastLock;
+
+        void OnEnable()
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            MulticastLock();
+#endif
+
+        }
 
         private async void Start()
         {
@@ -16,10 +28,10 @@ namespace SprayAR
         private async Task DiscoverServices()
         {
             string serviceType = "_http._tcp.local.";
-
+            Debug.Log("Discovering services...");
             // Discover services
             IReadOnlyList<IZeroconfHost> responses = await ZeroconfResolver.ResolveAsync(serviceType);
-
+            Debug.Log($"Discovered {responses.Count} services");
             foreach (var host in responses)
             {
                 foreach (var service in host.Services)
@@ -32,14 +44,66 @@ namespace SprayAR
                     }
                 }
             }
-        }
-
-        void Update()
-        {
-            // TODO: Currently, removal of services are not detected
+#if UNITY_ANDROID && !UNITY_EDITOR
+            // ReleaseMulticastLock();
+#endif
             foreach (var service in _discoveredServices)
             {
                 Debug.Log($"Service: {service}");
+            }
+        }
+
+        private async Task BrowseAllDomains()
+        {
+            ILookup<string, string> responses = await ZeroconfResolver.BrowseDomainsAsync();
+            Debug.Log($"Discovered {responses.Count} domains");
+            foreach (var domain in responses)
+            {
+                Debug.Log($"Domain: {domain.Key}");
+                foreach (var service in domain)
+                {
+                    Debug.Log($"Service: {service}");
+                }
+            }
+        }
+
+
+        void MulticastLock()
+        {
+            try
+            {
+                Debug.Log("Acquiring multicast lock...");
+                using (AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity"))
+                {
+                    using (var wifiManager = activity.Call<AndroidJavaObject>("getSystemService", "wifi"))
+                    {
+                        multicastLock = wifiManager.Call<AndroidJavaObject>("createMulticastLock", "Zeroconf lock");
+                        multicastLock.Call("acquire");
+                        Debug.Log("Multicast lock acquired");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed to acquire multicast lock: " + e.Message);
+            }
+        }
+
+        void ReleaseMulticastLock()
+        {
+            try
+            {
+                Debug.Log("Releasing multicast lock...");
+                if (multicastLock != null)
+                {
+                    multicastLock.Call("release");
+                    multicastLock = null;
+                    Debug.Log("Multicast lock released");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Failed to release multicast lock: " + e.Message);
             }
         }
     }
